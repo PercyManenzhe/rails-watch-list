2a35d8fc97b078cc12c08fe3ec07920348681707
Percy Manenzhe <percy@ptpi.co.za>
2023-05-13 19:03:14 +0200

    halfway_done_with_my_challenge

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :lists, only: %i[index show new create] do
     resources :bookmarks, only: %i[new create destroy]
     # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-    root to: "pages#home"
+    # root to: "pages#home"
     # Defines the root path route ("/")
   end
 end
